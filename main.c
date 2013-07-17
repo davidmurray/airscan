5754aa39c6ac7e3a9ca194c2257c2f564e387aa2
Cykey <cykey@live.com>
2013-07-17 18:12:42 -0400

    Asserts and verbose log.

--- main.c
+++ main.c
@@ -1,7 +1,7 @@
 // Airscan
 // By Cykey.
 
-/* TODO: -Add asserts() and do verbose mode thing. */
+/* TODO: - Pretty print network data. */
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -15,8 +15,16 @@ static WiFiDeviceClientRef device;
 static bool verbose_mode = false;
 static bool scan_mode = false;
 
+static inline void verbose_log(char *log)
+{
+	if (verbose_mode) {
+		puts(log);
+	}
+}
+
 static void scan_callback(WiFiDeviceClientRef device, CFArrayRef results, CFErrorRef error, void *unknown)
 {
+	verbose_log("Finished scan.");
 	CFShow(results);
 
 	CFRunLoopStop(CFRunLoopGetCurrent());
@@ -24,17 +32,24 @@ static void scan_callback(WiFiDeviceClientRef device, CFArrayRef results, CFErro
 
 static void begin_scan()
 {
+	verbose_log("Creating WiFiManagerClient...");
 	manager = WiFiManagerClientCreate(kCFAllocatorDefault, 0);
+	assert(manager != NULL);
 	CFArrayRef devices = WiFiManagerClientCopyDevices(manager);
-    device = (WiFiDeviceClientRef)CFArrayGetValueAtIndex(devices, 0);
-    CFRelease(devices);
+	assert(devices != NULL);
+	verbose_log("Getting WiFiDeviceClient...");
+	device = (WiFiDeviceClientRef)CFArrayGetValueAtIndex(devices, 0);
+	assert(device != NULL);
+	CFRelease(devices);
 
-    WiFiManagerClientScheduleWithRunLoop(manager, CFRunLoopGetCurrent(), kCFRunLoopDefaultMode);
+	verbose_log("Scheduling manager client with run loop...");
+	WiFiManagerClientScheduleWithRunLoop(manager, CFRunLoopGetCurrent(), kCFRunLoopDefaultMode);
 
-    // It is necessary to pass an empty dictionary (NULL does not work).
-    CFDictionaryRef options = CFDictionaryCreate(NULL, NULL, NULL, 0, NULL, NULL);
-    WiFiDeviceClientScanAsync(device, options, scan_callback, 0);
-    CFRelease(options);
+	// It is necessary to pass an empty dictionary (NULL does not work).
+	CFDictionaryRef options = CFDictionaryCreate(NULL, NULL, NULL, 0, NULL, NULL);
+	verbose_log("Starting to scan...");
+	WiFiDeviceClientScanAsync(device, options, scan_callback, 0);
+	CFRelease(options);
 }
 
 static void print_usage(char *progname)
