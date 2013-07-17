e587ed7a951858b11910212b6ceec5a081177854
Cykey <cykey@live.com>
2013-07-17 13:15:56 -0400

    Alright working utility.

--- Makefile
+++ Makefile
@@ -1,7 +1,11 @@
+export ADDITIONAL_CFLAGS = -I$(THEOS_PROJECT_DIR)/ios-reversed-headers
+
 include theos/makefiles/common.mk
 
 TOOL_NAME = airscan
 airscan_FILES = main.c
+airscan_FRAMEWORKS = CoreFoundation
 airscan_PRIVATE_FRAMEWORKS = MobileWiFi
+airscan_CODESIGN_FLAGS = -Sentitlements.plist
 
 include $(THEOS_MAKE_PATH)/tool.mk
--- main.c
+++ main.c
@@ -1,13 +1,41 @@
 // Airscan
 // By Cykey.
 
+/* TODO: -Add asserts() and do verbose mode thing. */
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
 #include <unistd.h>
+#include <CoreFoundation/CoreFoundation.h>
+#include <MobileWiFi/MobileWiFi.h>
 
+static WiFiManagerRef manager;
+static WiFiDeviceClientRef device;
 static bool verbose_mode = false;
 static bool scan_mode = false;
+
+static void scan_callback(WiFiDeviceClientRef device, CFArrayRef results, CFErrorRef error, void *unknown)
+{
+	CFShow(results);
+
+	CFRunLoopStop(CFRunLoopGetCurrent());
+}
+
+static void begin_scan()
+{
+	manager = WiFiManagerClientCreate(kCFAllocatorDefault, 0);
+	CFArrayRef devices = WiFiManagerClientCopyDevices(manager);
+    device = (WiFiDeviceClientRef)CFArrayGetValueAtIndex(devices, 0);
+    CFRelease(devices);
+
+    WiFiManagerClientScheduleWithRunLoop(manager, CFRunLoopGetCurrent(), kCFRunLoopDefaultMode);
+
+    // It is necessary to pass an empty dictionary (NULL does not work).
+    CFDictionaryRef options = CFDictionaryCreate(NULL, NULL, NULL, 0, NULL, NULL);
+    WiFiDeviceClientScanAsync(device, options, scan_callback, 0);
+    CFRelease(options);
+}
 
 static void print_usage(char *progname)
 {
@@ -36,6 +64,11 @@ int main(int argc, char *argv[])
 				print_usage(argv[0]);
 				exit(0);
 			}
+	}
+
+	if (scan_mode) {
+		begin_scan();
+		CFRunLoopRun();
 	}
 
 	return 0;
